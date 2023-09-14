b529c619ef26ce665ca442a9c6b62680b946b431
Ishikawa Ryota <lhasryota@gmail.com>
2023-09-14 09:04:18 +0900

    [add]マイクロタスクとマクロタスクを確認するスクリプトを追加 [updare]READEMEを更新

---
+++ scripts/script17.js
@@ -0,0 +1,20 @@
+new Promise(function promise(resolve) {
+  console.log('promise');
+
+  setTimeout(function task1() {
+    console.log('task1');
+    resolve();
+  });
+
+}).then(function job1() {
+  console.log('job1');
+  setTimeout(function task1() {
+    console.log('task2');
+  });
+}).then(function job2() {
+  console.log('job2');
+}).then(function job3() {
+  console.log('job3');
+})
+
+console.log('global end');
---
+++ scripts/script19.js
@@ -0,0 +1,31 @@
+new Promise(function promise(resolve) {
+  console.log('promise');
+
+  setTimeout(function task1() {
+    console.log('task1');
+    resolve();
+  });
+
+  setTimeout(function task1() {
+    console.log('task2');
+    
+    // queueMicrotask(function job4() {
+    //   console.log('job4')
+    // })
+
+    // queueMicrotaskと同じように書く方法
+    const p = Promise.resolve();
+    p.then(function job4() {
+      console.log('job4')
+    });
+  });
+
+}).then(function job1() {
+  console.log('job1');
+}).then(function job2() {
+  console.log('job2');
+}).then(function job3() {
+  console.log('job3');
+})
+
+console.log('global end');
